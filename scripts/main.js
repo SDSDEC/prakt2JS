cd141c79861e118efb04c638a79bf3a2497cac0c
SDSDEC <4elovekluna121206@gmail.com>
2026-03-01 15:58:43 +0300

    forma

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
 new Vue({
     el: '#app',
     template: `
-        
+        <div>
+            <div class="create-card">
+                <h3>Создать карточку</h3>
+                <div>
+                    <label>Заголовок:</label>
+                    <input v-model="newCardTitle" placeholder="Заголовок">
+                </div>
+                <div>
+                    <label>Пункты:</label>
+                    <div v-for="(item, index) in newCardItems" :key="index">
+                        <input v-model="newCardItems[index]" placeholder="Пункт">
+                        <button @click="removeItem(index)" v-if="newCardItems.length > 3">Удалить</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     `,
+    data: {
+        columns: [
+            { name: 'Нужно выполнить (макс.3)', cards: [] },
+            { name: 'Выполняется (макс.5)', cards: [] },
+            { name: 'Выполнено', cards: [] }
+        ],
+    }
 })
